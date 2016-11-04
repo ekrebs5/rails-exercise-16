15da7ade7434d10f500e1b5fa7625b30a4371b35
eva.krebs <eva.krebs@student.hpi.de>
2016-11-04 11:41:42 +0100

    test for #5 better

--- spec/views/authors/new.html.erb_spec.rb
+++ spec/views/authors/new.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "authors/new.html.erb", type: :view do
   it "Should have First name Field" do
-    render :template =>"authors/index.html.erb"
-    expect(rendered).to have_field('First_name')
+    render :template =>"authors/new.html.erb"
+    expect(rendered).to have_field('First name')
   end
 end
