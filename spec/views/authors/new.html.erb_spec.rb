6fc10e463461216092f7ff118c9ca0ee8f3cf030
eva.krebs <eva.krebs@student.hpi.de>
2016-11-04 11:51:53 +0100

    test for #7

--- spec/views/authors/new.html.erb_spec.rb
+++ spec/views/authors/new.html.erb_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe "authors/new.html.erb", type: :view do
     render :template =>"authors/new.html.erb"
     expect(rendered).to have_field('Last name')
   end
+  it "Should have Homepage Field" do
+    render :template =>"authors/new.html.erb"
+    expect(rendered).to have_field('Homepage')
+  end
 end
