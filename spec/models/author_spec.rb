fd2945ad9a9695e75c7961de09762f3dbb42a412
eva.krebs <eva.krebs@student.hpi.de>
2016-11-04 13:41:39 +0100

    test for #13

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Author, type: :model do
   it "Should create author" do
     build(:author)
   end
+  it "Should return full name" do
+    @author = build(:author)
+    expect(@author.name).to eq("Alan Turing")
+  end
 end
