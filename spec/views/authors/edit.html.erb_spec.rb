c65b93e216a2ada3c050694b9d2fe6889be6559f
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-08 18:28:21 +0100

    Failing test for #24

---
+++ spec/views/authors/edit.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "authors/edit.html.erb", type: :view do
+  before(:each) do
+    @author = create(:author)
+    assign(:author, @author)
+    render
+  end
+
+  it "Should have First name Field" do
+    expect(rendered).to have_field('First name')
+  end
+  it "Should have Last name Field" do
+    expect(rendered).to have_field('Last name')
+  end
+  it "Should have Homepage Field" do
+    expect(rendered).to have_field('Homepage')
+  end
+  it "Should have Create Author Submit Button" do
+    expect(rendered).to have_button('Save author')
+  end
+end
