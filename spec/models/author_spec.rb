9fa4bb35846c5c11430429e1d981e5f0312fb031
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-08 17:41:54 +0100

    Failing test for #21

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,8 +4,14 @@ RSpec.describe Author, type: :model do
   it "Should create author" do
     build(:author)
   end
+
   it "Should return full name" do
     @author = build(:author)
     expect(@author.name).to eq("Alan Turing")
+  end
+
+  it "Should not validate without last name" do
+    @author = Author.new(first_name: 'Alan', homepage: 'http://wikipedia.org/Alan_Turing')
+    expect(@author).to_not be_valid
   end
 end
