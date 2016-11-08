b3068169fd7bcbb42733dace8c76145cc6cb8a10
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-08 16:37:32 +0100

    Fix #17

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,6 +12,9 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  def index
+  end
+
   private
     def author_params
       params.require(:author).permit(:first_name, :last_name, :homepage)
