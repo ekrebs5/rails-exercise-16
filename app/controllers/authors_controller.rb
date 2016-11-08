b0fd874b6b7b9acab1b6c3e3df230acd8b41ec96
eva.krebs <eva.krebs@student.hpi.de>
2016-11-08 15:50:33 +0100

    fix #15

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,6 +8,9 @@ class AuthorsController < ApplicationController
     redirect_to new_author_path
   end
 
+  def show
+  end
+
   private
     def author_params
       params.require(:author).permit(:first_name, :last_name, :homepage)
