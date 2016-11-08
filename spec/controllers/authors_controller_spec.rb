a205328da474db6d2fb0d19ade498d80cdd3f412
eva.krebs <eva.krebs@student.hpi.de>
2016-11-08 15:49:41 +0100

    correct failing test for #15

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe AuthorsController, type: :controller do
   end
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, id:1
       expect(response).to have_http_status(:success)
     end
   end
