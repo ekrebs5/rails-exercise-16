3fa2a49307a36f49cafbba91325f17debd0dd99a
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-08 17:23:15 +0100

    Failing test for #19

--- spec/views/authors/index.html.erb_spec.rb
+++ spec/views/authors/index.html.erb_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "authors/index.html.erb", type: :view do
-  it "Should display authors name" do
+  before(:each) do
     @author = create(:author)
     assign(:authors, Author.all)
     render :template =>"authors/index.html.erb"
+  end
+
+  after(:each) do
+    Author.delete(@author)
+  end
+
+  it "Should display authors name" do
     expect(rendered).to have_text(@author.name)
+  end
+
+  it "Should display link to new author page" do
+    expect(rendered).to have_css('a', :text => 'Add Author')
   end
 end
