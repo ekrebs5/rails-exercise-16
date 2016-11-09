ae36978e3ac0c60cd9ca318753a117b1247ec162
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-09 15:39:29 +0100

    Failing test for #30

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "Should not validate without title" do
+      @paper = Paper.new(venue: "mind 49: 433-460", year: 1950)
+      expect(@paper).to_not be_valid
+  end
 end
