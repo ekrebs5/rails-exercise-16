7eb80a175d99e8b55d90380330a0e7ee9e2b803b
eva.krebs <eva.krebs@student.hpi.de>
2016-11-09 15:07:06 +0100

    test for and fix #29, generated scaffold paper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :papers
   # get 'authors/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104122932) do
+ActiveRecord::Schema.define(version: 20161109135820) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "homepage"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20161104122932) do
     t.datetime "updated_at", null: false
     t.string   "first_name"
     t.string   "last_name"
+  end
+
+  create_table "papers", force: :cascade do |t|
+    t.string   "title"
+    t.string   "venue"
+    t.integer  "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
