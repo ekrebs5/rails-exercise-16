d990079aeaad0821017aaa7240dc8a3530f89498
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-09 16:15:12 +0100

    Failing test for #34

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -9,17 +9,22 @@ RSpec.describe Paper, type: :model do
   end
 
   it "Should not validate without venue" do
-        @paper = Paper.new(title: "Add interesting title here", year: 1950)
-        expect(@paper).to_not be_valid
+    @paper = Paper.new(title: "Add interesting title here", year: 1950)
+    expect(@paper).to_not be_valid
   end
 
   it "Should not validate without year" do
-        @paper = Paper.new(title: "Add interesting title here", venue: "mind 49: 433-460")
-        expect(@paper).to_not be_valid
+    @paper = Paper.new(title: "Add interesting title here", venue: "mind 49: 433-460")
+    expect(@paper).to_not be_valid
   end
 
   it "Should not validate with non-integer year" do
-          @paper = Paper.new(title: "Add interesting title here", venue: "mind 49: 433-460", year: "ninety-nine")
-          expect(@paper).to_not be_valid
+      @paper = Paper.new(title: "Add interesting title here", venue: "mind 49: 433-460", year: "ninety-nine")
+      expect(@paper).to_not be_valid
+  end
+
+  it "Should have author list" do
+    @paper = Paper.new
+    expect(@paper.authors).not_to be_nil
   end
 end
