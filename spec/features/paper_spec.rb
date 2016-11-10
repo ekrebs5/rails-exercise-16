9b9c4fdb66b7143118141dba740b3ffb3f689a82
eva.krebs <eva.krebs@student.hpi.de>
2016-11-10 18:36:12 +0100

    fix #39

---
+++ spec/features/paper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Paper page", :type => :feature do
+
+pending "save correct author test"
+
+#  it "Should save paper with set authors" do
+    #@paper = create(:paper)
+    #visit edit_paper_path @paper
+
+  #  click_button "Create Paper"
+  #  Paper.find_by!()
+  #end
+
+end
