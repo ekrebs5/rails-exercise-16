e3d7002419d72db361911d3b6c3310072b78f95a
eva.krebs <eva.krebs@student.hpi.de>
2016-11-10 18:58:55 +0100

    test for #40

--- spec/factories/papers.rb
+++ spec/factories/papers.rb
@@ -5,4 +5,10 @@ FactoryGirl.define do
     year 1950
     authors {build_list :author, 1}
   end
+  factory :paper2, class: :paper do
+    title "go to statement considered harmful"
+    venue "Mind 49: 433-460"
+    year 1968
+    authors {build_list :author, 1}
+  end
 end
--- spec/features/paper_spec.rb
+++ spec/features/paper_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 describe "Paper page", :type => :feature do
 
+it "Should show correct paper if year is given in url" do
+  paper = create(:paper)
+  create(:paper2)
+  visit papers_path paper, :year => 1950
+  expect(page).to_not have_text("go to statement considered harmful")
+end
+
 pending "save correct author test"
 
 #  it "Should save paper with set authors" do
